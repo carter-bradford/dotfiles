797b2028651b36b96f5d86d17a03ab308387f3bf
cbradford@jasperridge.com <cbradford@02lwarford1.jasperridge.com>
2025-02-21 06:18:00 -0600

    Update to show hidden files in snacks

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = function(_, opts)
+    opts.picker = opts.picker or {}
+    opts.picker.sources = opts.picker.sources or {}
+    opts.picker.sources.files = opts.picker.sources.files or {}
+    opts.picker.sources.files.hidden = true
+    opts.picker.sources.explorer = opts.picker.sources.explorer or {}
+    opts.picker.sources.explorer.hidden = true
+  end,
+}
