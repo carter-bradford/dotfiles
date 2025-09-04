53ee52d2e75eaf07a9be1f15e2b2dc9f7aa36754
Carter Bradford <carter.bradford@gmail.com>
2025-09-04 09:41:54 -0500

    Updates to fix theme issue

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -5,6 +5,14 @@ return {
     name = "catppuccin",
     priority = 1000,
 
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+
     config = function()
       require("catppuccin").setup({
         transparent_background = true,
