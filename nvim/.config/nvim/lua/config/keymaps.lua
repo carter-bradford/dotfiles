d71fbb061406a1a60f21ef155e4d2ee3c35c1a1e
Carter Bradford <carter.bradford@gmail.com>
2026-02-04 06:04:57 -0600

    Nvim and app updates

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
+-- Normal & Visual mappings
+vim.keymap.set({ "n", "v" }, "<leader>ac", "<cmd>CodeCompanionChat<cr>", { desc = "AI: Open Chat" })
+vim.keymap.set("n", "<leader>ak", "<cmd>CodeCompanion<cr>", { desc = "AI: Inline Assistant" })
+-- Inline with prompt after selection (visual mode)
+vim.keymap.set("v", "<leader>ak", ":CodeCompanion ", { desc = "AI: Inline Assistant with Prompt" })
