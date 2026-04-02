ab6f70413eb9c37b9d74c71b2518862a6f8b444f
Carter Bradford <carter.bradford@gmail.com>
2026-04-02 09:53:27 -0500

    nvim updates

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,11 +3,18 @@ return {
   opts = function(_, opts)
     opts.picker = opts.picker or {}
     opts.picker.sources = opts.picker.sources or {}
-    opts.picker.sources.files = opts.picker.sources.files or {}
+    opts.picker.sources.files = opts.picker.sources.files or { hidden = true }
     opts.picker.sources.files.hidden = true
-    opts.picker.sources.explorer = opts.picker.sources.explorer or {}
+    opts.picker.sources.explorer = opts.picker.sources.explorer or { hidden = true }
     opts.picker.sources.explorer.hidden = true
+
     opts.image = opts.image or {}
     opts.dashboard = { enabled = false }
+
+    opts.lazygit = opts.lazygit or {}
+    opts.lazygit.theme = vim.tbl_deep_extend("force", opts.lazygit.theme or {}, {
+      activeBorderColor = { fg = "MatchParen", bold = true },
+      inactiveBorderColor = { fg = "99a4bf" },
+    })
   end,
 }
